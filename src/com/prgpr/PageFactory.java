4e90b802ecb5922692402b50c867ebfe5a03cb66
Kyle Rinfreschi <krinfreschi@msn.com>
2016-10-21 20:59:06 +0200

    Implementation of Page class

--- src/com/prgpr/PageFactory.java
+++ src/com/prgpr/PageFactory.java
@@ -4,9 +4,6 @@ import java.util.Set;
 import java.util.LinkedHashSet;
 import java.util.concurrent.ConcurrentHashMap;
 
-// import com.prgpr.Page;
-import com.prgpr.mock.Page;
-
 /**
  * Created by strange on 10/21/16.
  */
