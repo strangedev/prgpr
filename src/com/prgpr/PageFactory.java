9ec85bae51d8672986699c48d5a570734887e1cd
Noah Hummel <strange.dev@gmail.com>
2016-10-21 17:18:31 +0200

    Added com.prgpr.mock package for mock classes, added it to .gitignore

---
+++ src/com/prgpr/PageFactory.java
@@ -0,0 +1,18 @@
+package com.prgpr;
+
+import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.concurrent.ConcurrentHashMap;
+
+// import com.prgpr.Page;
+
+/**
+ * Created by strange on 10/21/16.
+ */
+public class PageFactory {
+
+    private ConcurrentHashMap<Long, Set<String>> categoriesForArticles;
+
+    public LinkedHashSet<Page>;
+
+}
