9500fa5d265b7b82d6911cd9c13c22e0409e5b75
Noah Hummel <strange.dev@gmail.com>
2016-10-23 01:04:10 +0200

    Added ThreadedArticleCollector
    Refactored WikiPageParser to work wth the new threaded implementation.

---
+++ src/com/prgpr/ThreadedArticleCollector.java
@@ -0,0 +1,162 @@
+package com.prgpr;
+
+import com.prgpr.collections.Tuple;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Stack;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.prgpr.WikiPageParser;
+import com.prgpr.collections.Tuple;
+import com.prgpr.Page;
+
+/**
+ * Created by strange on 10/22/16.
+ *
+ */
+public class ThreadedArticleCollector implements Runnable, Iterator<ArrayList<Tuple<Page, String>>>{
+
+    private Thread t;
+
+    private final ReentrantLock lock = new ReentrantLock();
+
+    private int chunkSize;
+    private String infilePath;
+    private WikiPageParser pageParser;
+    private ArrayList<Tuple<Page, String>> nextArticleChunk;
+    private Stack<ArrayList<Tuple<Page, String>>> articleChunks;
+    private static final Logger log = LogManager.getFormatterLogger(ThreadedArticleCollector.class);
+
+    /**
+     * Constructor.
+     *
+     * @param chunkSize The amount of articles to be collected before making them available
+     *                  For streaming to another thread.
+     */
+    ThreadedArticleCollector(String infilePath, int chunkSize) {
+        log.info("New thread created.");
+
+        this.chunkSize = chunkSize;
+        this.infilePath = infilePath;
+        pageParser = new WikiPageParser();
+        this.nextArticleChunk = new ArrayList<>(this.chunkSize);
+
+        lock.lock();
+        articleChunks = new Stack<>();
+        lock.unlock();
+
+    }
+
+    /**
+     * Parses one line of the input by calling WikiPageParser.parseLine()
+     * and aggregates parsed articles into chunks.
+     *
+     * @param line A line of input to be parsed
+     */
+    private void delegateParseLine(String line) {
+
+        boolean articleComplete = pageParser.parseLine(line);
+
+        if (articleComplete) { // add to current chunk
+
+            try {
+
+                Tuple<Page, String> protoPage = pageParser.getProtoPage();
+                nextArticleChunk.add(protoPage);
+
+            } catch (IllegalAccessException exception) {
+
+                log.error(exception.getMessage());
+                // @todo how to handle this properly?
+
+            }
+
+            if (nextArticleChunk.size() >= chunkSize) { // check if chunk has been filled - if so, add to ArrayList
+
+                lock.lock();
+                articleChunks.push(nextArticleChunk);
+                lock.unlock();
+
+                nextArticleChunk.clear(); // clear next chunk, we'll be filling it again
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Executes the thread.
+     *
+     */
+    public void run() {
+        log.info("Thread running.");
+
+        // Do the deed.
+
+        try (Stream<String> stream = Files.lines(Paths.get(infilePath))) {
+            stream.forEachOrdered(this::delegateParseLine);
+
+        }
+        catch (IOException exception) {
+            log.error("Couldn't get lines of file: " + infilePath);
+
+        }
+
+        log.info("Thread stopped.");
+
+    }
+
+    /**
+     * Starts the thread.
+     *
+     */
+    public void start () {
+        log.info("Thread started.");
+
+        if (t == null) { // There can only be one.
+            t = new Thread (this, this.getClass().getName());
+            t.start ();
+
+        }
+
+    }
+
+    /**
+     * Getter method for chunkSize
+     *
+     * @return The chunkSize with which the thread was set up.
+     */
+    public int getChunkSize() {
+        return chunkSize;
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        // @todo synchronize
+        lock.lock();
+        boolean ret = articleChunks.size() > 0; // @todo is .size() really O(1)?
+        lock.unlock();
+
+        return ret;
+    }
+
+    @Override
+    public ArrayList<Tuple<Page, String>> next() {
+        // @todo synchronize
+        lock.lock();
+        ArrayList<Tuple<Page, String>> ret = articleChunks.pop();
+        lock.unlock();
+
+        return ret;
+    }
+
+}
--- src/com/prgpr/WikiPageParser.java
+++ src/com/prgpr/WikiPageParser.java
@@ -5,81 +5,102 @@ import com.prgpr.exceptions.MalformedWikidataException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class WikiPageParser {
 
     private static final Logger log = LogManager.getFormatterLogger(WikiPageParser.class);
     private static final Character articleDelimiter = '¤';
 
-    public LinkedList<Tuple<Page, String>> aggregatedProtoPages = new LinkedList<>();
-
-    private LinkedList<String> aggregatedLines = new LinkedList<>();
     private boolean delimiterEncountered = false;
-    private long tmpPageId;
-    private int tmpNamespaceId;
-    private String tmpTitle;
+
+    private ArrayList<String> aggregatedLines = new ArrayList<>();
     private Page tmpPage = null;
 
-    private Page createProtoPageInstance(long id, int namespaceId, String title) {
-        return new Page(id, namespaceId, title);
+    public Tuple<Page, String> getProtoPage() throws IllegalAccessException {
+        if (protoPage == null) {
+            log.error("protoPage accessed before creation");
+            throw new IllegalAccessException("protoPage accessed before creation");
+        }
+        return protoPage;
     }
 
-    public void parseLine(String line) throws MalformedWikidataException {
+    private Tuple<Page, String> protoPage;
+
+    public boolean parseLine(String line) throws MalformedWikidataException {
 
         if(line.length() > 0) {
 
-            if(line.charAt(0) == articleDelimiter){
+            if(line.charAt(0) == articleDelimiter){ // Delimiter encountered.
 
-                if(delimiterEncountered) {
+                if(delimiterEncountered) { // There was already a delimiter in a previous line, the article is complete.
 
-                    if(tmpPage == null){
+                    if(tmpPage == null){ // If there was an error before, we can't complete this article.
                         log.error("Malformed article encountered");
                         throw new MalformedWikidataException();
                     }
 
-                    aggregatedLines.stream()
-                            .reduce((x, y) -> x + y)
-                            .ifPresent(x -> aggregatedProtoPages.add(new Tuple<>(tmpPage, x)));
+                    Optional<String> maybeArticle = aggregatedLines.stream()
+                            .reduce((x, y) -> x + y);
 
-                } else {
+                    if (maybeArticle.isPresent()) {
+                        protoPage = new Tuple<>(tmpPage, maybeArticle.get());
 
-                    String[] attributeStrings = line.split("\\s+");
+                    } else {
+                        log.error("Empty article encountered");
+                        throw new MalformedWikidataException("Empty article encountered");
 
-                    if (attributeStrings.length < 1) {
+                    }
+
+
+                    // A complete article has been parsed.
+                    return true;
+
+                } else { // This is a new delimiter, negate delimiterEncountered and parse some info.
+
+                    String[] attributeStrings = line.split("\\s+"); // strip whitespace from attributes line
+
+                    // Attribute string needs to be wll formed, were not trying to do error correction.
+                    if (attributeStrings.length < 4) {
                         log.error("Page attributes are missing");
                         throw new MalformedWikidataException("Page attributes are missing");
                     }
 
+                    long pageId;
                     try {
-                        tmpPageId = Long.parseLong(attributeStrings[1]);
+                        pageId = Long.parseLong(attributeStrings[1]);
                     } catch (NumberFormatException exception) {
                         log.error("Page attribute 'pageId' is malformed");
                         throw new MalformedWikidataException("Page attribute 'pageId' is malformed");
                     }
 
+                    int namespaceId;
                     try {
-                        tmpNamespaceId = Integer.parseInt(attributeStrings[2]);
+                        namespaceId = Integer.parseInt(attributeStrings[2]);
                     } catch (NumberFormatException exception) {
                         log.error("Page attribute 'namespaceId' is malformed");
                         throw new MalformedWikidataException("Page attribute 'namespaceId' is malformed");
                     }
 
-                    tmpTitle = attributeStrings[3];
-                    tmpPage = this.createProtoPageInstance(tmpPageId, tmpNamespaceId, tmpTitle);
-
+                    String title = attributeStrings[3];
+                    tmpPage = new Page(pageId, namespaceId, title); // prepare page object, this will be given to other
+                                                                    // parsers alongside with the article contents.
+                                                                    // @todo Fix responsibilities
                 }
 
-                delimiterEncountered = !delimiterEncountered;
+                delimiterEncountered = !delimiterEncountered; // Flip the flag!
 
             }
 
         }
 
-        if(delimiterEncountered){
+        if(delimiterEncountered){ // If we're inside of an article, aggregate all teh linez.
             aggregatedLines.add(line);
 
         }
+
+        return false;
 
     }
 }
