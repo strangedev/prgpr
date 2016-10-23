ce19a78e0697b7e6f7559b2f5eafd279fcb03cf3
Noah Hummel <strange.dev@gmail.com>
2016-10-23 20:32:42 +0200

    Started testing PageFactory

---
+++ src/com/prgpr/PageFactoryTest.java
@@ -0,0 +1,29 @@
+package com.prgpr;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.prgpr.PageFactory;
+
+import java.util.Set;
+
+/**
+ * Created by strange on 10/23/16.
+ */
+public class PageFactoryTest {
+
+    @Test
+    public void extractPages() throws Exception {
+
+        Set<Page> pagesContained = PageFactory.extractPages("res/infile/wikipedia_de_prgpr_subset.txt");
+        System.out.print("Found: ");
+        System.out.print(pagesContained.size());
+        System.out.print(" pages.");
+
+
+    }
+
+}
