0bdbe368862195483afc60daae9b94cba1396a5e
Noah Hummel <strange.dev@gmail.com>
2016-11-09 18:08:09 +0100

    Updated git2svn

--- tools/git2svn/git2svn.sh
+++ tools/git2svn/git2svn.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
-cd $1
+SVN_DIR=$1
+COMMIT_MSG=$2
+INFILE=$3
+
+cd ${SVN_DIR}
 rm -rf ./*
 git clone "https://github.com/strangedev/prgpr.git"
-cd ./prgpr
-git checkout Production
+
+cd prgpr
+sh ./gradlew build
+sh ./gradlew javadoc
 cd ../
-mv ./prgpr/* ./
-rm -rf ./prgpr
-sed -i -e 's/datas/data/g' *
-sed -i -e 's/Datas/Data/g' *
+
+mv prgpr/README.txt ./README.txt
+mv prgpr/settings.gradle ./settings.gradle
+mv prgpr/build.gradle ./build.gradle
+mv prgpr/gradlew ./gradlew
+mv prgpr/gradlew.bat ./gradlew.bat
+mv prgpr/CapnWikicrunch.sh ./CapnWikicrunch.sh
+mv prgpr/src ./src
+mv prgpr/build/docs ./docs
+mv prgpr/build/libs/CapnWikicrunch* ./
+
+mkdir example_output
+sh ./CapnWikicrunch.sh ${INFILE} ./example_output/out.xml true
+mv events.log ./example_output/events.log
+
+zip example_output.zip ./example_output/*
+rm -rf ./example_output
+
 svn add ./*
-svn commit -m $2
+svn commit -m ${COMMIT_MSG}
 svn update
 
