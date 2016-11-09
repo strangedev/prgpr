61d0768a6465e2735c49484f72f873ea053b4ca1
Kyle Rinfreschi <krinfreschi@msn.com>
2016-11-09 21:40:43 +0100

    update shell scripts

--- CapnWikicrunch.sh
+++ CapnWikicrunch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-JAVA_FILE="CapnWikicrunch.jar"
+FILE_PATH=${1:-"."}
+JAVA_FILE="${FILE_PATH}/CapnWikicrunch.jar"
 VM_PARAMS="-Xmx500m"
 java ${VM_PARAMS} -jar ./${JAVA_FILE} $@
--- tools/git2svn/git2svn.sh
+++ tools/git2svn/git2svn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ROOT_DIR=`realpath $1`
-COMMIT_MSG=$2
-INFILE=`realpath $3`
+COMMIT_MSG=$1
+INFILE=`realpath $2`
+ROOT_DIR=`realpath ${3:-external_files/personal_stash/svn_checkout}`
 
 GIT_DIR="git"
 GIT_REPO="ssh://git@github.com/strangedev/prgpr.git"
@@ -10,6 +10,10 @@ GIT_REPO="ssh://git@github.com/strangedev/prgpr.git"
 SVN_DIR="svn"
 
 JAVA_FILE="CapnWikicrunch.jar"
+
+if ! [ -d "$ROOT_DIR" ]; then
+    mkdir -p ${ROOT_DIR}
+fi
 
 cd ${ROOT_DIR}
 
@@ -37,18 +41,20 @@ cp ${GIT_DIR}/gradlew ${SVN_DIR}/gradlew
 cp ${GIT_DIR}/gradlew.bat ${SVN_DIR}/gradlew.bat
 cp ${GIT_DIR}/CapnWikicrunch.sh ${SVN_DIR}/CapnWikicrunch.sh
 cp -R ${GIT_DIR}/src ${SVN_DIR}/src
-cp -R ${GIT_DIR}/build/docs ${SVN_DIR}/docs
+cp -R ${GIT_DIR}/config ${SVN_DIR}/config
+cp -R ${GIT_DIR}/build/docs/javadoc ${SVN_DIR}/javadoc
 cp ${GIT_DIR}/build/libs/${JAVA_FILE} ${SVN_DIR}/
 
 cd ${SVN_DIR}
-
 mkdir example_output
-java -jar -Xmx500m ${JAVA_FILE} ${INFILE} ./example_output/out.xml true
+echo "Running CapnWikicrunch Jar"
+./CapnWikicrunch.sh ${INFILE} ./example_output/out.xml true >> /dev/null
+tail -2 events.log
 mv events.log ./example_output/events.log
 
 zip example_output.zip ./example_output/*
 rm -rf ./example_output
 
-#svn add ./*
-#svn commit -m ${COMMIT_MSG}
-#svn update
+svn add ./*
+svn commit -m ${COMMIT_MSG}
+svn update
