e218a6f3c86fa2259a3671bee2a01af4c1d86a19
Kyle Rinfreschi <krinfreschi@msn.com>
2016-11-09 21:59:04 +0100

    new log for every run

--- tools/compare.sh
+++ tools/compare.sh
@@ -2,8 +2,10 @@
 
 RAW_DATA=`realpath $1`
 XML_FILE=`realpath $2`
+LOG_FILE=`realpath $3`
 
 echo "Pages in data file : "  $(cat ${RAW_DATA} | egrep "¤\s[0-9]+\s[0-9]+\s.*" | wc -l)
 echo "Pages found: " $(cat ${XML_FILE} | egrep "<page\s" | wc -l)
 echo "Categories found: " $(cat ${XML_FILE} | egrep "<category\s" | wc -l)
 echo "Total number of lines: " $(cat ${XML_FILE} | wc -l)
+echo "Log number of lines: " $(cat ${LOG_FILE} | wc -l)
