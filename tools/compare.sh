59d95ece1c2b322ba5e00f253585bc138f40ca43
Kyle Rinfreschi <krinfreschi@msn.com>
2016-12-02 16:03:52 +0100

    Reduced hashcode collisions

--- tools/compare.sh
+++ tools/compare.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 RAW_DATA=`realpath $1`
-XML_FILE=`realpath $2`
-LOG_FILE=`realpath $3`
+LOG_FILE=`realpath $2`
+#XML_FILE=`realpath $3`
 
 echo "Pages in data file : "  $(cat ${RAW_DATA} | egrep "¤\s[0-9]+\s[0-9]+\s.*" | wc -l)
-echo "Pages found: " $(cat ${XML_FILE} | egrep "<page\s" | wc -l)
-echo "Categories found: " $(cat ${XML_FILE} | egrep "<category\s" | wc -l)
-echo "Total number of lines: " $(cat ${XML_FILE} | wc -l)
+echo "Duplicates in data file: " $(cat ${RAW_DATA} | egrep "¤\s[0-9]+\s[0-9]+\s.*" | awk '{$1=""; $2=""; sub("  ", " "); print}' | uniq -c | awk '$1 > 1 {print}' | wc -l)
 echo "Log number of lines: " $(cat ${LOG_FILE} | wc -l)
+#echo "Pages found: " $(cat ${XML_FILE} | egrep "<page\s" | wc -l)
+#echo "Categories found: " $(cat ${XML_FILE} | egrep "<category\s" | wc -l)
+#echo "Total number of lines: " $(cat ${XML_FILE} | wc -l)
