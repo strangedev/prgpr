7cef5ce5b99cd9fc64de76c28d9dedb2354eca08
Noah Hummel <strange.dev@gmail.com>
2016-12-10 12:44:33 +0100

    Added logging to EmbeddedDatabaseFactory

--- src/com/prgpr/framework/database/EmbeddedDatabaseFactory.java
+++ src/com/prgpr/framework/database/EmbeddedDatabaseFactory.java
@@ -1,21 +1,26 @@
 package com.prgpr.framework.database;
 
 import com.prgpr.framework.database.neo4j.Neo4jEmbeddedDatabase;
-import com.prgpr.framework.database.neo4j.Neo4jTransactionFactory;
 import com.prgpr.framework.database.transaction.BatchTransactionManager;
-import com.prgpr.framework.database.transaction.SimpleTransactionManager;
 import com.prgpr.framework.database.transaction.TransactionManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.neo4j.graphdb.GraphDatabaseService;
 
 /**
  * Created by kito on 19.11.16.
  */
 public class EmbeddedDatabaseFactory {
+
+    private static final Logger log = LogManager.getFormatterLogger(EmbeddedDatabaseFactory.class);
+
     public static EmbeddedDatabase newEmbeddedDatabase(String path){
+        log.info("Establishing new database connection...");
         return new Neo4jEmbeddedDatabase(path);
     }
 
     public static EmbeddedDatabase newEmbeddedDatabase(String path, int transactionBatchSize){
+        log.info("Establishing new database connection...");
         Neo4jEmbeddedDatabase db = new Neo4jEmbeddedDatabase(path);
         TransactionManager tm =new BatchTransactionManager(db.getTransactionManager(), transactionBatchSize);
         db.setTransactionManager(tm);
@@ -23,6 +28,7 @@ public class EmbeddedDatabaseFactory {
     }
 
     public static EmbeddedDatabase newEmbeddedDatabase(GraphDatabaseService db) {
+        log.info("Establishing new database connection...");
         return new Neo4jEmbeddedDatabase(db);
     }
 }
