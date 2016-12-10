307b1be29d84b90c665981945a9ce0ae0bdcef4d
Noah Hummel <strange.dev@gmail.com>
2016-12-10 13:35:31 +0100

    Minor Docs...

--- src/com/prgpr/LinkExtraction.java
+++ src/com/prgpr/LinkExtraction.java
@@ -62,12 +62,12 @@ public class LinkExtraction {
 
         try {
             Document articles = Jsoup.parse(article);
-            Elements links = articles.select("a"); // As the files are too old, they don't have the format of the real wikipages
-            Pattern r = Pattern.compile("/wiki/([^:]*\")"); // Only the /wiki/articlename are the internal articles are taken
+            Elements links = articles.select("a");  // Because the files are too old, they don't have the format of the real wikipages
+            Pattern r = Pattern.compile("/wiki/([^:]*\")");  // Only the /wiki/articlename are the internal articles and are taken
 
             for (Element link : links) {
                 String test = link.toString();
-                Matcher m = r.matcher(test); // Parsing the link String to see if he is valid
+                Matcher m = r.matcher(test);  // Parsing the link String to see if he is valid
                 if (m.find()) { articleLinks.add(link.attr("title")); }
             }
         } catch (Exception except) {
